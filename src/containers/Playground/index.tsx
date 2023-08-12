9d46ea37b93426f1fe729822dcbc383afdf78b58
Aqmal <50930584+kifasu@users.noreply.github.com>
2023-08-13 01:08:17 +0800

    added react-skele and updated button props with customizable loader

--- src/containers/Playground/index.tsx
+++ src/containers/Playground/index.tsx
@@ -1,23 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { motion } from 'framer-motion';
 
+import { BUTTON } from 'entities/tailwind';
+
 import Button from 'components/Button';
+import Flex from 'components/Flex';
 import Modal from 'components/Modal';
 
 const MotionButton = motion(Button);
 
 const Playground = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isLoading === true) {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    }
+  }, [isLoading]);
+
+  const onButtonClick = () => {
+    setIsOpen(true);
+    setIsLoading(!isLoading);
+  };
+
   return (
     <div>
       <MotionButton
         label="test"
         fill={false}
-        onClick={() => setIsOpen(true)}
+        onClick={() => onButtonClick()}
         noPadding
+        variant={BUTTON.reset}
         whileHover={{ scale: 1.5 }}
+        isLoading={isLoading}
         whileTap={{ scale: 0.9 }}
+        customLoader={<div>loading ..</div>}
       />
       {isOpen && (
         <Modal onClick={() => setIsOpen(false)}>
@@ -25,7 +46,16 @@ const Playground = () => {
         </Modal>
       )}
 
-      <Button label="test" fill={false} onClick={() => setIsOpen(true)} />
+      <Button
+        label="test"
+        fill={false}
+        onClick={() => onButtonClick()}
+        isLoading={isLoading}
+      />
+      <Flex>
+        <div>test</div>
+        <div>lol</div>
+      </Flex>
     </div>
   );
 };
